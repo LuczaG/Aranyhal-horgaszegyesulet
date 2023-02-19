9268ebd5cbae63e4d1554346866bfe0955ac3b1b
LuczaG <gergo.lucza@gmail.com>
2023-02-19 14:20:08 +0100

    Made account.service and interceptors.

---
+++ src/app/_helpers/jwt.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from '@app/_services/account.service';
+import { environment } from '@environments/environment';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    constructor(private accountService: AccountService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // a felhasználóhoz hitelesítési fejlécet adunk a JwT-vel
+        // feltételek: bejelentkezés, a kérésnek az ApiUrl-re kell hivatkoznia
+        const user = this.accountService.userValue;
+        const isLoggedIn = user && user.token;
+        const isApiUrl = req.url.startsWith(environment.apiUrl);
+        if (isLoggedIn && isApiUrl) {
+            req = req.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            });
+        }
+        
+        return next.handle(req);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { HomeComponent } from './_components/home/home.component';
+import { fakeBackendProvider } from './_helpers/fake-backend';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { HomeComponent } from './_components/home/home.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    // szolágltató, amit a hamis backend létrehozására használunk
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
